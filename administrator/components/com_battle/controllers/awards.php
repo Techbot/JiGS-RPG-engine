c05bea7be6958c7e7f9523a9b8f7199f4329e61d
Justin Kelly <justinkelly@justinkelly.net>
2013-07-12 14:14:14 +0100

    Started on the awards backend.

---
+++ administrator/components/com_battle/controllers/awards.php
@@ -0,0 +1,64 @@
+<?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
+jimport('joomla.application.component.controller');
+
+class BattleControllerAwards extends JController
+{
+	function __construct ($config = array())
+	{
+		parent::__construct($config);
+		$this->registerTask('unpublish','publish');
+		$this->registerTask('apply','save');
+	}
+	function edit()
+	{
+		JRequest::setVar('view', 'award');
+		$this->display();
+	}
+	function add()
+	{
+		JRequest::setVar('view', 'award');
+		$this->display();
+	}
+	function save()
+	{
+		$row =& JTable::getInstance('awards', 'Table');
+		$cid = JRequest::getVar( 'cid', array(0), '', 'array' );
+		$row->id = $cid[0];
+		if (!$row->bind(JRequest::get('get'))) 
+		{
+			JError::raiseError(500, $row->getError() );
+		}
+		if (!$row->store()) 
+		{
+			JError::raiseError(500, $row->getError() );
+		}
+		
+		if(JRequest::getVar('task')=='apply')
+		{
+			$this->setRedirect('index.php?option=com_battle&controller=awards&task=edit&cid='.$row->id,
+				'Award Saved');
+		}
+		else
+		{
+			$this->setRedirect('index.php?option=com_battle&view=awards', 'Award Saved');
+		}
+	}
+	function display()
+	{
+		$view = JRequest::getVar('view');
+		if (!$view)
+		{
+			switch ($this->getTask())
+			{
+			case 'edit':
+				JRequest::setVar('view', 'award');
+				break;
+			default:
+				JRequest::setVar('view', 'awards');
+				break;
+			}
+		}
+		parent::display();
+	}
+}
