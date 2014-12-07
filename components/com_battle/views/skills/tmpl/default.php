bc6b8794a1acec418df621c73e2a034ee2813668
Lisa Corcoran <lcdemc@gmail.com>
2014-12-07 22:45:17 +0000

    add class on click with jquery, for selected skill, comment out test spinner

--- components/com_battle/views/skills/tmpl/default.php
+++ components/com_battle/views/skills/tmpl/default.php
@@ -31,6 +31,31 @@ No Primary Skill Selected
 
 
 <script type='text/javascript'>
+
+
+/* Add class "selected" to current primary skill */
+
+/*$( "body" ).append( "<div class='overlay'><div class='loader'>Loading...</div></div>" );*/
+
+$( document ).ready(function() {
+
+	//setTimeout(function() {
+	//      $(".overlay").remove();
+	//}, 400);
+
+
+	$( "#master_skills_table div, #primary_upgrade_table div" ).on( "click", function() {
+		//alert('works');
+		/* this adds class to selected div,and removes class from any other in the same section */
+		$( this ).toggleClass("selected").siblings().removeClass();
+		/* this adds removes class from any other in the other section */
+		$( this ).parent().siblings().children().removeClass();
+	});
+
+});
+
+
+
     var a = new Request.JSON(
         {
             url: "index.php?option=com_battle&format=raw&task=skills_action&action=get_available_skills&parent=1",
@@ -118,8 +143,7 @@ No Primary Skill Selected
         add_open();
     }
 
-
-
+     
 
 
 
