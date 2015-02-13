8caf4ffee92d51f835bbdaad2a1014bb78afb048
Lisa Corcoran <lcdemc@gmail.com>
2015-02-13 22:39:10 +0000

    remove inline styles from board_panel and account list, add class 'bank', remove embedded css with background image

--- components/com_battle/views/building/tmpl/default_board_primary_bank.php
+++ components/com_battle/views/building/tmpl/default_board_primary_bank.php
@@ -1,9 +1,6 @@
 
 <div class="name">Hackable Accounts List</div>
-<div id = "account_list" style = " background:black;
-                                    overflow-y:scroll;
-                                    height:260px;
-                                    ">
+<div id = "account_list">
     <!--Stat Points    <- 3 ->  <br />-->
 
 </div>
