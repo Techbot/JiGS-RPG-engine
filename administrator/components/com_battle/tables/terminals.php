c84c98ca3f5c6ec11ae25aa54c0e993c87ab6d80
techbot <djtechbot@gmail.com>
2015-04-01 22:20:22 +0200

    terminals table updated

--- administrator/components/com_battle/tables/terminals.php
+++ administrator/components/com_battle/tables/terminals.php
@@ -5,15 +5,23 @@ defined('_JEXEC') or die('Restricted access');
 class TablePages extends JTable
 {
     var $id         = null;
-    var $comment    = null;
- /*   var $posy       = null;
+    var $grid       = null;
     var $posx       = null;
+    var $posy       = null;
     var $image      = null;
+    var $comment    = null;
     var $type       = null;
     var $level      = null;
-    var $grid       = null;
+    var $ip         = null;
+    var $domiain    = null;
+    var $bandwidth  = null;
+    var $capacity   = null;
+    var $version    = null;
+    var $status     = null;
+    var $faction    = null;
+    var $battery    = null;
     var $published	= null;
-*/
+
 
     function __construct(&$db)
     {
