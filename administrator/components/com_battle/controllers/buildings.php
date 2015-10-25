408db31705357da597b764a07211ccf8cbaaeb85
techbot <djtechbot@gmail.com>
2015-10-25 13:07:41 +0100

    dunno

--- administrator/components/com_battle/controllers/buildings.php
+++ administrator/components/com_battle/controllers/buildings.php
@@ -14,6 +14,7 @@ class BattleControllerbuildings extends JControllerLegacy
         JRequest::setVar('view', 'building');
         $this->display();
     }
+
     function add()
     {
         JRequest::setVar('view', 'building');
