a7e4772b794fc526f6f2aae41ccc1314acf70f55
techbot <djtechbot@gmail.com>
2015-12-01 21:55:20 +0100

    backend fixes

--- administrator/components/com_battle/views/person/view.html.php
+++ administrator/components/com_battle/views/person/view.html.php
@@ -5,15 +5,16 @@ JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_battle/tables');
 
 class battleViewperson extends JViewLegacy
 {
-	function display($tpl = null)
-	{
-		$row =& JTable::getInstance('people', 'Table');
-		$cid = JRequest::getVar( 'cid', array(0), '', 'array' );
-		$id = $cid[0];
-		$row->load($id);
-		$this->assignRef('row', $row);
-		$editor =& JFactory::getEditor();
-		$this->assignRef('editor', $editor);
-		parent::display($tpl);
-	}
+    function display($tpl = null)
+    {
+        $row = JTable::getInstance('people', 'Table');
+        $cid = JRequest::getVar( 'cid', array(0), '', 'array' );
+        $id = $cid[0];
+        $row->load($id);
+        $this->assignRef('row', $row);
+        $editor = JFactory::getEditor();
+        $this->assignRef('editor', $editor);
+        parent::display($tpl);
+    }
 }
+
