e76dda49659c066f1afc59f144ff81e17e7ca8f7
Lisa Corcoran <lcdemc@gmail.com>
2016-06-19 13:34:04 +0200

    refactor into states, resize canvas

--- components/com_battle/views/plate/tmpl/027_hobbits/default.php
+++ components/com_battle/views/plate/tmpl/027_hobbits/default.php
@@ -1,24 +1,18 @@
 <html>
-	<head>
-		<script src="/components/com_battle/views/phaser/tmpl/_site/js/jquery-2.0.3.min.js"></script>
-		<!-- Phaser -->
-        <script src="/components/com_battle/views/phaser/tmpl/_site/js/phaser.js"></script>
-		
-		<style>	
-			body {
-				margin: 0;
-			}
-		</style>	
-	</head>
-	
-	<body>
-			
-		<div id="world">
-		</div>
-
-	<script src='/components/com_battle/views/plate/tmpl/027_hobbits/script.js'></script>
-
-	</body>
+<head>
+  <!-- Phaser -->
+  <script src="/components/com_battle/includes/phaser.js"></script>
+  <style>
+    body {
+      margin: 0;
+    }
+  </style>
+</head>
+<body>
+<div id="world">
+</div>
+<script src='state001.js'></script>
+<script src='script.js'></script>
+</body>
 </html>
-
 
