9a30b1d698fef21ac289eaa87ef9f2572f0124dd
Techbot <djtechbot@gmail.com>
2023-10-03 16:27:43 +0200

    add skills vue wireframe

--- client/src/components/pages/Skills.vue
+++ client/src/components/pages/Skills.vue
@@ -9,11 +9,59 @@ export default {
     return {
       dataModel: [
         {
-          id: "numberOrString",
+          id: "Skills",
           label: "Root Node",
+          treeNodeSpec: {
+            state: {
+              expanded: true,
+            }
+          },
           children: [
-            {id: 1, label: "Child Node"},
-            {id: "node2", label: "Second Child"}
+            {
+              id: 1, label: "Lock Picking", treeNodeSpec: {
+                state: {
+                  expanded: true,
+                }
+              },
+              children: [
+                { id: 1, label: "Level One" }, { id: 1, label: "Level Two" },
+                { id: 1, label: "Level Three" }, { id: 1, label: "Level Four" }
+              ]
+            },
+
+            {
+              id: "node2", label: "Melee", treeNodeSpec: {
+                state: {
+                  expanded: true,
+                }
+              },
+              children: [
+                { id: 1, label: "Level One" }, { id: 1, label: "Level Two" },
+                { id: 1, label: "Level Three" }, { id: 1, label: "Level Four" }
+              ]
+            },
+            {
+              id: 1, label: "Range", treeNodeSpec: {
+                state: {
+                  expanded: false,
+                }
+              },
+              children: [
+                { id: 1, label: "Level One" }, { id: 1, label: "Level Two" },
+                { id: 1, label: "Level Three" }, { id: 1, label: "Level Four" }
+              ]
+            },
+            {
+              id: "node2", label: "Heal", treeNodeSpec: {
+                state: {
+                  expanded: false,
+                }
+              },
+              children: [
+                { id: 1, label: "Level One" }, { id: 1, label: "Level Two" },
+                { id: 1, label: "Level Three" }, { id: 1, label: "Level Four" }
+              ],
+            }
           ]
         }
       ]
@@ -24,5 +72,5 @@ export default {
 
 
 <template>
-  <tree-view  id="my-tree" :initial-model="dataModel"></tree-view>
+  <tree-view id="my-tree" :initial-model="dataModel"></tree-view>
 </template>
