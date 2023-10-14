1ca40fb6730bebdedbced64ff9aa8290754c051d
techbot <djtechbot@gmail.com>
2023-10-14 14:06:53 +0200

    HUD position, formatting

--- client/src/game/scenes/HudScene.ts
+++ client/src/game/scenes/HudScene.ts
@@ -45,81 +45,79 @@ export class HudScene extends Scene {
     }
     create() {
 
+      // Dialogue.
         this.thing = this.createTextBox(this, 10, 380, {
             wrapWidth: 500,
         }).start(this.jigs.content, 50).setDepth(7);
 
-
-        var r1 = this.add.rectangle(80, 60, 260, 100, 0x6666ff).setBlendMode(Phaser.BlendModes.MULTIPLY);
-        var r2 = this.add.rectangle(560, 60, 260, 100, 0x6666ff).setBlendMode(Phaser.BlendModes.MULTIPLY);
-
-        //  Our Text object to display the Score
-        let info = this.add.text(10, 10, 'Credits: ', { font: '12px Arial', fill: '#ffffff' });
+        // HUD1 bg
+        var r1 = this.add.rectangle(10, 10, 190, 120, 0x6666ff).setDisplayOrigin(0, 0).setBlendMode(Phaser.BlendModes.MULTIPLY);
+        // HUD2 bg
+        var r2 = this.add.rectangle(730, 10, 160, 90, 0x6666ff).setDisplayOrigin(0, 0).setBlendMode(Phaser.BlendModes.MULTIPLY);
 
         //  Grab a reference to the Game Scene
         let ourGame = this.scene.get('main');
         //  Listen for events from it
 
         ourGame.events.on('addScore', function () {
-            this.score += 10;
+          this.score += 10;
         }, this);
 
         ourGame.events.on('content', function () {
-            this.thing.destroy();
-            this.thing = this.createTextBox(this, 10, 380, {
-                wrapWidth: 500,
-            }).start(this.jigs.content, 50).setDepth(7)
-
-
+          this.thing.destroy();
+          this.thing = this.createTextBox(this, 10, 380, {
+              wrapWidth: 500,
+          }).start(this.jigs.content, 50).setDepth(7)
         }, this);
 
         ourGame.events.on('position', function (x: number, y: number) {
             this.x = x;
             this.y = y;
             info.setText('Credits: ' + this.jigs.playerStats.credits);
-
         }, this);
 
-        // this.hud1 = this.add.text(10, 16, '', { fontSize: '24px', fill: '#111111' });
-        this.hud2 = this.add.text(10, 28, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud3 = this.add.text(10, 46, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud4 = this.add.text(10, 66, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud5 = this.add.text(10, 86, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud11 = this.add.text(10, 106, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud12 = this.add.text(10, 126, '', { font: '12px Arial', fill: '#ffffff' });
+        //  Our Text object to display the Score
+        let info = this.add.text(15, 15, 'Credits: ', { font: '12px Arial', fill: '#ffffff' });
+        this.hud2 = this.add.text(15, 30, '', { font: '12px Arial', fill: '#ffffff'});
+        this.hud3 = this.add.text(15, 45, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud4 = this.add.text(15, 60, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud5 = this.add.text(15, 75, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud11 = this.add.text(15, 90, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud12 = this.add.text(15, 105, '', { font: '12px Arial', fill: '#ffffff' });
 
-        this.hud6 = this.add.text(460, 28, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud7 = this.add.text(460, 46, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud8 = this.add.text(460, 66, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud9 = this.add.text(460, 86, '', { font: '12px Arial', fill: '#ffffff' });
-        this.hud10 = this.add.text(460, 106, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud6 = this.add.text(735, 15, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud7 = this.add.text(735, 30, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud8 = this.add.text(735, 45, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud9 = this.add.text(735, 60, '', { font: '12px Arial', fill: '#ffffff' });
+        this.hud10 = this.add.text(735, 75, '', { font: '12px Arial', fill: '#ffffff' });
 
     }
     update() {
-        // this.hud1.setText('Blue: ' +  this.jigs.Blobby);
+        // HUD1
         this.hud2.setText('State: ' + this.jigs.gameState);
         this.hud3.setText('Node: ' + this.jigs.userMapGrid);
         this.hud4.setText('TileMap: ' + this.jigs.tiled);
         this.hud5.setText('Title: ' + this.jigs.nodeTitle);
+        this.hud11.setText('Name: ' + this.jigs.playerName);
+        this.hud12.setText('Id: ' + this.jigs.playerId);
+
+        // HUD2
         this.hud6.setText('X: ' + this.x + 'Y: ' + this.y);
 
         if (this.jigs.portalsArray[0]) {
-            this.hud7.setText('P1 X: ' + this.jigs.portalsArray[0].x + 'P1 Y: ' + this.jigs.portalsArray[0].y);
+          this.hud7.setText('P1 X: ' + this.jigs.portalsArray[0].x + 'P1 Y: ' + this.jigs.portalsArray[0].y);
         }
 
         if (this.jigs.portalsArray[1]) {
-            this.hud8.setText('P2 X: ' + this.jigs.portalsArray[1].x + 'P2 Y: ' + this.jigs.portalsArray[1].y);
+          this.hud8.setText('P2 X: ' + this.jigs.portalsArray[1].x + 'P2 Y: ' + this.jigs.portalsArray[1].y);
         }
 
         if (this.jigs.portalsArray[2]) {
-            this.hud9.setText('P3 X: ' + this.jigs.portalsArray[2].x + 'P3 Y: ' + this.jigs.portalsArray[2].y);
+          this.hud9.setText('P3 X: ' + this.jigs.portalsArray[2].x + 'P3 Y: ' + this.jigs.portalsArray[2].y);
         }
 
         this.hud10.setText('City: ' + this.jigs.city);
-        this.hud11.setText('Name: ' + this.jigs.playerName);
-        this.hud12.setText('Id: ' + this.jigs.playerId);
     }
-
 
     GetValue = Phaser.Utils.Objects.GetValue;
 
@@ -129,38 +127,39 @@ export class HudScene extends Scene {
         var fixedHeight = this.GetValue(config, 'fixedHeight', 0);
         var titleText = this.GetValue(config, 'title', undefined);
 
-
         var textBox = scene.rexUI.add.textBox({
-            x: x,
-            y: y,
+          x: x,
+          y: y,
 
-            background: scene.rexUI.add.roundRectangle({ radius: 20, color: this.COLOR_PRIMARY, strokeColor: this.COLOR_LIGHT, strokeWidth: 2 }),
+          background: scene.rexUI.add.roundRectangle({ radius: 20, color: this.COLOR_PRIMARY, strokeColor: this.COLOR_LIGHT, strokeWidth: 2 }),
 
-            icon: scene.rexUI.add.roundRectangle({ radius: 20, color: this.COLOR_DARK }),
+          icon: scene.rexUI.add.roundRectangle({ radius: 20, color: this.COLOR_DARK }),
 
-            // text: getBuiltInText(scene, wrapWidth, fixedWidth, fixedHeight),
-            text: this.getBBcodeText(scene, wrapWidth, fixedWidth, fixedHeight),
+          // text: getBuiltInText(scene, wrapWidth, fixedWidth, fixedHeight),
+          text: this.getBBcodeText(scene, wrapWidth, fixedWidth, fixedHeight),
 
-            action: scene.add.image(0, 0, 'nextPage').setTint(this.COLOR_LIGHT).setVisible(false),
+          action: scene.add.image(0, 0, 'nextPage').setTint(this.COLOR_LIGHT).setVisible(false),
 
-            title: (titleText) ? scene.add.text(0, 0, titleText, { fontSize: '24px', }) : undefined,
+          title: (titleText) ? scene.add.text(0, 0, titleText, { font: 'bold 24px Arial', fill: '#ffffff' }) : undefined,
 
-            separator: (titleText) ? scene.rexUI.add.roundRectangle({ height: 3, color: this.COLOR_DARK }) : undefined,
+          separator: (titleText) ? scene.rexUI.add.roundRectangle({ height: 3, color: this.COLOR_DARK }) : undefined,
 
-            space: {
-                left: 20, right: 20, top: 20, bottom: 20,
+          space: {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            icon: 10,
+            text: 10,
+            separator: 6,
+          },
 
-                icon: 10, text: 10,
-
-                separator: 6,
-            },
-
-            align: {
-                title: 'center'
-            }
+          align: {
+            title: 'center'
+          }
         })
-            .setOrigin(0)
-            .layout();
+          .setDisplayOrigin(0, 0)
+          .layout();
 
         textBox
             .setInteractive()
@@ -177,19 +176,19 @@ export class HudScene extends Scene {
             }, textBox)
             .on('pageend', function () {
                 if (this.isLastPage) {
-                    return;
+                  return;
                 }
 
                 var icon = this.getElement('action').setVisible(true);
                 this.resetChildVisibleState(icon);
                 icon.y -= 30;
                 var tween = scene.tweens.add({
-                    targets: icon,
-                    y: '+=30', // '+=100'
-                    ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
-                    duration: 500,
-                    repeat: 0, // -1: infinity
-                    yoyo: false
+                  targets: icon,
+                  y: '+=30', // '+=100'
+                  ease: 'Bounce', // 'Cubic', 'Elastic', 'Bounce', 'Back'
+                  duration: 500,
+                  repeat: 0, // -1: infinity
+                  yoyo: false
                 });
             }, textBox)
             .on('complete', function () {
@@ -202,22 +201,26 @@ export class HudScene extends Scene {
     }
 
     getBuiltInText = function (scene, wrapWidth, fixedWidth, fixedHeight) {
-        return scene.add.text(0, 0, '', {
-            fontSize: '20px',
-            wordWrap: {
-                width: wrapWidth
-            },
-            maxLines: 3
-        })
-            .setFixedSize(fixedWidth, fixedHeight);
+      return scene.add.text(0, 0, '', {
+        font: ' bold 24px Arial',
+        fontWeight: 'bold',
+        fontSize: '24px',
+        fill: '#ffffff',
+        wordWrap: {
+            width: wrapWidth
+        },
+        maxLines: 3
+      }).setFixedSize(fixedWidth, fixedHeight);
     }
 
     getBBcodeText = function (scene, wrapWidth, fixedWidth, fixedHeight) {
         return scene.rexUI.add.BBCodeText(0, 0, '', {
             fixedWidth: fixedWidth,
             fixedHeight: fixedHeight,
-
-            fontSize: '20px',
+            font: 'bold 24px Arial',
+            fontWeight: 'bold',
+            fontSize: '24px',
+            fill: '#ffffff',
             wrap: {
                 mode: 'word',
                 width: wrapWidth
@@ -234,8 +237,8 @@ export class HudScene extends Scene {
             height: 400,
 
             background: scene.rexUI.add.roundRectangle({
-                color: this.COLOR_PRIMARY,
-                radius: 20
+              color: this.COLOR_PRIMARY,
+              radius: 20
             }),
 
             // text: scene.add.text(),
@@ -243,22 +246,24 @@ export class HudScene extends Scene {
             // textMask: true,
 
             slider: {
-                track: scene.rexUI.add.roundRectangle(0, 0, 20, 10, 10, this.COLOR_DARK),
-                thumb: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 13, this.COLOR_LIGHT),
+              track: scene.rexUI.add.roundRectangle(0, 0, 20, 10, 10, this.COLOR_DARK),
+              thumb: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 13, this.COLOR_LIGHT),
             },
 
             space: {
-                left: 20, right: 20, top: 20, bottom: 20,
-
-                text: 10,
-                // text: {
-                //     top: 20,
-                //     bottom: 20,
-                //     left: 20,
-                //     right: 20,
-                // },
-                header: 20,
-                footer: 20,
+              left: 20,
+              right: 20,
+              top: 20,
+              bottom: 20,
+              text: 10,
+              // text: {
+              //     top: 20,
+              //     bottom: 20,
+              //     left: 20,
+              //     right: 20,
+              // },
+              header: 20,
+              footer: 20,
             },
 
             scroller: {
@@ -266,36 +271,36 @@ export class HudScene extends Scene {
             },
 
             mouseWheelScroller: {
-                focus: false,
-                speed: 0.1
+              focus: false,
+              speed: 0.1
             },
 
             header: scene.rexUI.add.label({
-                space: { left: 10, right: 10, top: 10, bottom: 10 },
+              space: { left: 10, right: 10, top: 10, bottom: 10 },
 
-                orientation: 0,
-                background: scene.rexUI.add.roundRectangle(0, 0, 20, 20, 0, this.COLOR_DARK),
-                text: scene.add.text(0, 0, 'Title'),
+              orientation: 0,
+              background: scene.rexUI.add.roundRectangle(0, 0, 20, 20, 0, this.COLOR_DARK),
+              text: scene.add.text(0, 0, 'Title'),
             }),
 
             footer: scene.rexUI.add.label({
-                space: { left: 10, right: 10, top: 10, bottom: 10 },
+              space: { left: 10, right: 10, top: 10, bottom: 10 },
 
-                orientation: 0,
-                background: scene.rexUI.add.roundRectangle({
-                    radius: 10,
-                    color: this.COLOR_DARK,
-                    strokeColor: this.COLOR_LIGHT
-                }),
-                text: scene.add.text(0, 0, 'Close'),
+              orientation: 0,
+              background: scene.rexUI.add.roundRectangle({
+                  radius: 10,
+                  color: this.COLOR_DARK,
+                  strokeColor: this.COLOR_LIGHT
+              }),
+              text: scene.add.text(0, 0, 'Close'),
             }).onClick(function (button, gameObject, pointer, event) {
-                gameObject.getTopmostSizer().modalClose();
+              gameObject.getTopmostSizer().modalClose();
             }),
 
             content: this.jigs.content,
 
             expand: {
-                footer: false
+              footer: false
             }
         })
     }
@@ -303,7 +308,7 @@ export class HudScene extends Scene {
     CreateContent = function (linesCount) {
         var numbers = [];
         for (var i = 0; i < linesCount; i++) {
-            numbers.push('[color=' + ((i % 2) ? 'green' : 'yellow') + ']' + i.toString() + '[/color]');
+          numbers.push('[color=' + ((i % 2) ? 'green' : 'yellow') + ']' + i.toString() + '[/color]');
         }
         return this.jigs.content + '\n' + numbers.join('\n');
     }
