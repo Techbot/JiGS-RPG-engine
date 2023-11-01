90e5eac46d66ccd7ddab0268923701cad79a1eae
techbot <djtechbot@gmail.com>
2023-11-01 13:25:03 +0100

    remove files from git cache

--- client/src/game/backend.ts
+++
@@ -1,5 +0,0 @@
-export const BACKEND_URL = (window.location.href.indexOf("192.168.178.206") === -1)
-    ? `${window.location.protocol.replace("http", "ws")}//${window.location.hostname}${(window.location.port && `:${window.location.port}`)}`
-    : "ws://192.168.178.206:2567"
-
-export const BACKEND_HTTP_URL = BACKEND_URL.replace("ws", "http");
