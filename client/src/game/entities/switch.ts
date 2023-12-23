88c127924c91e8fc01de99dfc7f9e1d8738db4d7
Techbot <djtechbot@gmail.com>
2023-12-23 15:03:35 +0100

    add interactive property

--- client/src/game/entities/switch.ts
+++ client/src/game/entities/switch.ts
@@ -1,12 +1,22 @@
 import Phaser from "phaser";
-
+import { useJigsStore } from '../../stores/jigs';
 export default class Switch extends Phaser.Physics.Arcade.Sprite {
+  jigs: any;
   constructor(scene, x, y) {
-    super(scene, x, y, 'ship');
-    this.pathIndex = 0;
+    super(scene, x, y, 'switch-001-lever-off');
+    this.jigs = useJigsStore();
+    const stuff = 0;
+    // this.pathIndex = 0;
+    this.setInteractive({ cursor: 'url(/assets/images/cursors/speak.cur), pointer' });
+    this.on('pointerdown', this.onSwitchDown.bind(this, stuff));
+
 
   }
   preUpdate(time, delta) {
     this.setDepth(6);
+  }
+
+  onSwitchDown(stuff) {
+    console.log('switch flicked')
   }
 }
