076a50cf71c1e5efec2a6d47d9a957a284226718
techbot <djtechbot@gmail.com>
2024-01-12 19:37:32 +0100

    wireframe layout

--- client/src/components/pages/Character.vue
+++ client/src/components/pages/Character.vue
@@ -3,8 +3,75 @@ import { useJigsStore } from "../../stores/jigs";
 </script>
 
 <template>
-  <video width="640" height="480" controls="false" autoplay="true"
-        playsinline="true" muted="" loop="">
-        <source src="/assets/images/Skeleton_All_SD.mp4" type="video/mp4">
-</video>
+
+
+  <div class="emc-layout emc-char">
+
+    <div class="emc-char__left">
+      <div class="emc-char__item">
+        <h2>Weapon</h2>
+        <select>
+          <option>Gun</option>
+          <option>Sword</option>
+        </select>
+      </div>
+
+      <div class="emc-char__item">
+        <h2>Ammo</h2>
+        <select>
+          <option>Fire</option>
+          <option>Ice</option>
+        </select>
+      </div>
+
+    </div>
+
+    <div class="emc-char__center">
+
+    </div>
+
+    <div class="emc-char__right">
+
+      <div class="emc-char__item">
+        <h2>Implant</h2>
+        <select>
+          <option>Eye</option>
+          <option>Brain</option>
+          <option>Skin</option>
+        </select>
+      </div>
+
+      <div class="emc-char__item">
+        <h2>Armour</h2>
+        <select>
+          <option>Suit of aromour</option>
+          <option>Kevlar vest</option>
+        </select>
+      </div>
+    </div>
+
+  </div>
 </template>
+
+<style scoped>
+  .emc-layout.emc-char {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 1rem;
+    background: pink;
+  }
+
+  .emc-layout.emc-char > div {
+    flex: 0 1 33%;
+    width: 33%;
+    padding: 1rem;
+  }
+
+  .emc-char__center {
+    flex: 1 0 50%;
+  }
+  .emc-char__item {
+    margin-bottom: 1rem;
+  }
+
+</style>
