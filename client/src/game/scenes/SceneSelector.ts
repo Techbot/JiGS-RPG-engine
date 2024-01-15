84cadf03202b1b856ea9726e69b81f4db32f4201
techbot <djtechbot@gmail.com>
2024-01-15 21:40:48 +0100

    remove list from scene selector, change cursor

--- client/src/game/scenes/SceneSelector.ts
+++ client/src/game/scenes/SceneSelector.ts
@@ -1,14 +1,14 @@
 import Phaser from "phaser";
-import WebFont from '../../assets/WebFont'
+// import WebFont from '../../assets/WebFont'
 
 export class SceneSelector extends Phaser.Scene {
 
-    parts = {
-        '1': "Help",
-        '2': "Credits",
-        '3': "Options",
-        '4': "HEADQUARTERS",
-    };
+    // parts = {
+    //     '1': "Help",
+    //     '2': "Credits",
+    //     '3': "Options",
+    //     '4': "HEADQUARTERS",
+    // };
 
     constructor() {
         super({ key: "selector" });
@@ -19,7 +19,7 @@ export class SceneSelector extends Phaser.Scene {
     preload() {
         // update menu background color
         this.cameras.main.setBackgroundColor(0x000000);
-        this.load.addFile(new WebFont(this.load, ['Roboto', 'Neutron Demo']))
+        // this.load.addFile(new WebFont(this.load, ['Roboto', 'Neutron Demo']))
         this.load.image('einstein', '/assets/images/8b0f172a-80ed-4fbd-b357-c512127970ce.png');
         // preload demo assets
         // this.load.image('ship_0001', 'assets/ship_0001.png');
@@ -29,9 +29,9 @@ export class SceneSelector extends Phaser.Scene {
 
     create() {
         this.image = this.add.image(480, 320, 'einstein')
-            .setInteractive().
+            .setInteractive({ cursor: 'url(/assets/images/cursors/speak.cur), pointer' }).
             on("pointerdown", () => {
-                this.game.scene.switch("selector", 'myScene');
+                this.game.scene.switch("selector", 'main');
             });
 
 
@@ -41,17 +41,17 @@ export class SceneSelector extends Phaser.Scene {
             fontFamily: "Neutron Demo"
         };
 
-        for (let partNum in this.parts) {
-            const index = parseInt(partNum) - 1;
-            const label = this.parts[partNum];
+        // for (let partNum in this.parts) {
+        //     const index = parseInt(partNum) - 1;
+        //     const label = this.parts[partNum];
 
-            // this.add.text(32, 32 + 32 * index, `Part ${partNum}: ${label}`, textStyle)
-            this.add.text(80, 100 + 70 * index, `${partNum}: ${label}`, textStyle)
-                .setInteractive()
-                .setPadding(6)
-                .on("pointerdown", () => {
-                    this.game.scene.switch("selector", "main");
-                });
-        }
+        //     // this.add.text(32, 32 + 32 * index, `Part ${partNum}: ${label}`, textStyle)
+        //     this.add.text(80, 100 + 70 * index, `${partNum}: ${label}`, textStyle)
+        //         .setInteractive()
+        //         .setPadding(6)
+        //         .on("pointerdown", () => {
+        //             this.game.scene.switch("selector", "main");
+        //         });
+        // }
     }
 }
