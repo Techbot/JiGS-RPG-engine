27bb147894aff9af3325409b32f62d5ccff8d45e
techbot <djtechbot@gmail.com>
2024-01-23 11:41:58 +0100

    add handler for position update to drones as a group

--- client/src/game/entities/drones.ts
+++ client/src/game/entities/drones.ts
@@ -4,15 +4,42 @@
 import Drone from "./drone";
 import { useJigsStore } from '../../stores/jigs';
 
-export default class Drones  {
+export default class Drones {
   jigs: any;
   dronesGroup: any;
 
-  constructor(self,x,y) {
-    this.dronesGroup = self.physics.add.group({ allowGravity: false });
-    this.dronesGroup.add(new Drone(self, x, y, 100, 100, 0.005),true);
-    this.dronesGroup.add(new Drone(self, x, y, 40, 100, 0.005), true);
-    this.dronesGroup.add(new Drone(self, x, y, 40, 100, -0.005), true);
+  constructor(scene, x, y) {
+    this.dronesGroup = scene.physics.add.group({ allowGravity: false });
+    this.dronesGroup.add(new Drone(scene, x, y, 100, 100, 0.005), true);
+    this.dronesGroup.add(new Drone(scene, x, y, 40, 100, 0.005), true);
+    this.dronesGroup.add(new Drone(scene, x, y, 40, 100, -0.005), true);
+
+    scene.events.on('position', this.handler, this);
+
   }
+
+  handler(x, y) {
+    if (this.dronesGroup.children) {
+      this.dronesGroup.children.each(function (drone) {
+        drone.update(x, y);
+      }, this);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
