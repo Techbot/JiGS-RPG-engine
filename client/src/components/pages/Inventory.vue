140872b3be070aa7bde1873a8b1b5b04e0156262
techbot <djtechbot@gmail.com>
2024-02-13 17:49:51 +0100

    styles directly in vue file

--- client/src/components/pages/Inventory.vue
+++ client/src/components/pages/Inventory.vue
@@ -88,3 +88,51 @@ export default {
     </div>
   </div>
 </template>
+<style>
+/* Inventory */
+.inventory h3 {
+  margin-top: 1rem;
+}
+
+.inventory img {
+  margin-bottom: 1rem;
+}
+
+.list-group {
+  display: flex;
+  padding: 0.5rem;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-content: flex-start;
+  height: 100%;
+  min-height: 184px; /* Two rows */
+  background-color: #111;
+}
+
+.list-group-item {
+  max-width: 20%;
+  min-height: 80px;
+  flex: 1 0 20%;
+  border: 0 none;
+  border-radius: 0;
+  text-align: center;
+  font-size: 0.75rem;
+  padding: 0.25rem;
+  background-color: #000;
+}
+
+.list-group-item:hover,
+.list-group-item.sortable-chosen {
+  border: 2px solid red;
+  background: #222;
+}
+.list-group-item.sortable-chosen {
+  box-shadow: 0px 0px 0px 5px #111111, inset 0px 10px 27px -8px #141414, inset 0px -10px 27px -8px #A31925, 5px 5px 15px 5px rgba(0,0,0,0);
+}
+
+.list-group-item:first-child,
+.list-group-item:last-child {
+  border-radius: 0;
+}
+</style>
