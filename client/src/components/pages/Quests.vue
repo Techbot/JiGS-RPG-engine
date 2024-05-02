d4ec2cf12cb5ef54b155462e29ceb7584fb1d298
techbot <djtechbot@gmail.com>
2024-05-02 18:06:26 +0200

    styles from main, refine and add thumbnails

--- client/src/components/pages/Quests.vue
+++ client/src/components/pages/Quests.vue
@@ -5,7 +5,8 @@ import * as coreui from '@coreui/coreui'
 export default {
   components: {
     coreui
-  }, setup() {
+  },
+  setup() {
     const jigs = useJigsStore();
     return {
       jigs,
@@ -22,6 +23,7 @@ export default {
       .get("/mymissions?_wrapper_format=drupal_ajax")
       .then((response) => {
         this.jigs.playerQuests = response.data[0].value["playerMissions"];
+        console.log(this.jigs.playerQuests)
       });
   }
 }
@@ -29,14 +31,15 @@ export default {
 
 <template>
   <div class="quests">
-    <!-- <img src="/assets/images/header2.png" /> -->
+    <img src="/assets/images/header2.png" />
     <div class="accordion accordion-flush" id="accordionFlushExample" v-for="quest in jigs.playerQuests.quests" :key="quest.id">
-      <div class="accordion-item">
+      <div class="quest accordion-item">
+        <img class="quest__thumbnail" src="/assets/images/System/quest.png" alt="quest type thumbnail" />
         <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button"
+          <button class="quest__heading accordion-button collapsed" type="button"
             data-coreui-toggle="collapse" data-coreui-target="#flush-collapseOne"
             aria-expanded="false" aria-controls="flush-collapseOne">
-              Id: {{ quest.id }} => Name: {{ quest.title ? quest.title : 'Quest Title' }}
+              {{ quest.id }}: {{ quest.title ? quest.title : 'Quest Title' }}
           </button>
         </h2>
         <div id="flush-collapseOne" class="accordion-collapse collapse"
@@ -47,3 +50,69 @@ export default {
     </div>
   </div>
 </template>
+
+<style>
+  .quest__heading {
+    color: #fff;
+    background-color: var(--emc-black);
+    font-family: Neutron Demo;
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+
+  .quests .quest {
+    background: var(--emc-black);
+    color: white;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+
+  .quest__thumbnail {
+    flex: 0 1 34px;
+    margin-right: 0.25rem;
+  }
+
+  .quests .accordion-button {
+    background: var(--emc-black);
+    border: 4px solid transparent;
+    color: white;
+  }
+
+  .accordion.accordion-flush {
+    margin-bottom: 2rem;
+  }
+
+  .accordion-collapse {
+    flex: 1 0 100%;
+  }
+
+  .accordion-body {
+    background-color: var(--emc-black-rich);
+  }
+
+  .accordion-button:hover,
+  .accordion-button:focus {
+    border: 4px solid var(--emc-teal-dark-alt);
+  }
+
+  .accordion-button:focus {
+    border-color: var(--emc-teal-alt);
+    outline: 0;
+  }
+
+  .accordion-button::after {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23ffffff'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
+  }
+
+  .accordion-button:not(.collapsed) {
+    color: white !important;
+    background-color: var(--emc-black) !important;
+    box-shadow: none;
+  }
+
+  .accordion-button:not(.collapsed)::after {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23ffffff'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
+  }
+
+</style>
--- client/src/components/pages/Skills.vue
+++ client/src/components/pages/Skills.vue
@@ -20,7 +20,7 @@ export default {
               name: 'checkbox1'
             },
             state: {
-              expanded: false,
+              expanded: true,
               selected: false,
               input: {
                 value: false,
@@ -127,3 +127,213 @@ export default {
 <template>
   <tree-view id="my-tree" :initial-model="dataModel"></tree-view>
 </template>
+
+<style>
+  /* Skills tree */
+  .grtv-wrapper.grtv-default-skin > .grtv {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    background-color: #111;
+    padding: 1rem;
+    font-size: 1.25rem;
+  }
+
+  /* Everything's in a .grtv-wrapper (embedded CSS is the 'grtv-default-skin' skin) */
+  .grtv-wrapper.grtv-default-skin {
+    --baseHeight: 2rem;
+    --itemSpacing: 2rem;
+  }
+
+  /* The node, including its content and children list */
+  .grtv-wrapper.grtv-default-skin .grtvn {
+    padding-left: 0;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn:first-child {
+    margin-top: 0;
+  }
+
+  /* ARIA styles */
+  .grtv-wrapper.grtv-default-skin .grtvn[role="treeitem"]:focus {
+    outline: 0;
+  }
+
+  /* .grtv-wrapper.grtv-default-skin .grtvn[role="treeitem"]:focus >.grtvn-self {
+    outline: black dotted 1px;
+  } */
+
+  /* The node's content, excluding the list of child nodes */
+  .grtv-wrapper.grtv-default-skin .grtvn-self {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1rem;
+    /* line-height: 2rem; */
+    margin-bottom: 1rem;
+  }
+  .grtv-wrapper.grtv-default-skin .grtvn-children .grtvn-children .grtvn-self {
+    margin-bottom: 0;
+  }
+  /* Drag and Drop styles */
+  .grtv-wrapper.grtv-default-skin .grtvn-dragging .grtvn-self {
+    opacity: 0.5;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-drop-target {
+    flex-wrap: wrap;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-drop-target.grtvn-self-child-drop-target {
+    opacity: .5;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-drop-target .grtvn-self-sibling-drop-target {
+    width: 100%;
+    height: 7px;
+    background-color: #dddddd;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-drop-target .grtvn-self-sibling-drop-target.grtvn-self-sibling-drop-target-hover {
+    background-color: #bbbbbb;
+  }
+
+  /* The expander button and indicator content */
+  .grtv-wrapper.grtv-default-skin .grtvn-self-expander {
+    /* padding: 0; */
+    /* background: none; */
+    /* border: none; */
+    /* height: var(--baseHeight); */
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-expander i.grtvn-self-expanded-indicator {
+    font-style: normal;
+    color: white;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-expander i.grtvn-self-expanded-indicator::before {
+    content: '+';
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-expander.grtvn-self-expanded i.grtvn-self-expanded-indicator::before {
+    content: '-';
+  }
+
+  /* The styling for when the node is selected */
+  .grtv-wrapper.grtv-default-skin .grtvn-self-selected {
+    background-color: #f0f0f8;
+  }
+
+  /* Spacing */
+  .grtv-wrapper.grtv-default-skin .grtvn-self-expander,
+  .grtv-wrapper.grtv-default-skin .grtvn-self-checkbox,
+  .grtv-wrapper.grtv-default-skin .grtvn-self-radio,
+  /* .grtv-wrapper.grtv-default-skin .grtvn-self-spacer, */
+  .grtv-wrapper.grtv-default-skin .grtvn-self-action {
+    min-width: 1rem;
+    flex: 0 1 2rem;
+    background-color: var(--emc-teal-alt);
+    font-weight: 700;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    border: 4px solid transparent;
+  }
+
+  .grtvn-self-expander:hover,
+  .grtvn-self-expander:focus {
+    border: 4px solid var(--emc-teal-dark-alt);
+  }
+  .grtvn-self-expander:focus {
+    border-color: var(--emc-teal-alt);
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-expander,
+  .grtv-wrapper.grtv-default-skin .grtvn-self-spacer {
+    margin: 0;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-checkbox,
+  .grtv-wrapper.grtv-default-skin .grtvn-self-radio {
+    /* margin: 0 0 0 calc(-1 * var(--itemSpacing)); */
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-self-text,
+  .grtv-wrapper.grtv-default-skin .grtvn-self-label {
+    /* margin-left: var(--itemSpacing); */
+  }
+
+  .grtvn-self-label {
+    font-family: "Neutron Demo";
+    font-size: 1.75rem;
+  }
+
+  /* Action buttons section */
+  .grtv-wrapper.grtv-default-skin .grtvn-self-action {
+    /* padding: 0; */
+    /* background: none; */
+    /* border: none; */
+    /* height: var(--baseHeight); */
+  }
+
+  /* Action buttons (add, delete, etc) */
+  .grtv-wrapper.grtv-default-skin i.grtvn-self-add-child-icon {
+    font-style: normal;
+  }
+
+  .grtv-wrapper.grtv-default-skin i.grtvn-self-add-child-icon::before {
+    content: '+';
+  }
+
+  .grtv-wrapper.grtv-default-skin i.grtvn-self-delete-icon {
+    font-style: normal;
+    color: white;
+  }
+
+  .grtv-wrapper.grtv-default-skin i.grtvn-self-delete-icon::before {
+    content: 'x';
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn-children-wrapper {
+    margin: 0 0 0 calc(1rem + var(--itemSpacing));
+  }
+
+  /* The node's child list */
+  .grtv-wrapper.grtv-default-skin .grtvn-children {
+    padding: 0;
+    list-style: none;
+    margin-bottom: 2rem;
+  }
+
+  .grtv-wrapper.grtv-default-skin .grtvn.grtvn-hidden {
+    display: none;
+  }
+
+  .grtvn-children-wrapper > ul > li > .grtvn-children-wrapper > .grtvn-children {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-column-gap: 1rem;
+    grid-row-gap: 1rem;
+    grid-template-areas:s
+      "level1 level2 level3 level4"
+  }
+
+  .grtvn-children-wrapper > ul > li > .grtvn-children-wrapper > .grtvn-children > li {
+    background-color: var(--emc-dark);
+  }
+  .grtv-wrapper.grtv-default-skin .grtvn-self-action {
+    background-color: var(--emc-black);
+  }
+  .grtvn-self-action:hover,
+  .grtvn-self-action:focus {
+    border: 4px solid var(--emc-black-rich);
+  }
+  .grtvn-self-action:focus {
+    border-color: var(--emc-teal-alt);
+  }
+
+
+
+</style>
