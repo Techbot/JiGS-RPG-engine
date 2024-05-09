61d0d6674c00e7ebad5a29edb22b9c2fb8bbf62c
techbot <djtechbot@gmail.com>
2024-05-09 09:35:16 +0200

    add walk128

--- client/src/game/scenes/BootScene.ts
+++ client/src/game/scenes/BootScene.ts
@@ -9,15 +9,12 @@ export class BootScene extends Scene {
   }
 
   preload () {
-   // this.load.image('bomb', '/assets/images/bomb.png');
-   // this.load.image('sky', '/assets/images/sky.png');
-  /*   this.load.audio('thud', [thudMp3, thudOgg]) */
     this.load.spritesheet('PsibotF', '/assets/images/sprites/avatars/1000.png', { frameWidth: 64, frameHeight: 64 });
     this.load.spritesheet('PsibotF_slash', '/assets/images/sprites/avatars/1000.png', { frameWidth: 192, frameHeight: 192 });
+    this.load.spritesheet('PsibotF_walk128', '/assets/images/sprites/avatars/1000.png', { frameWidth: 128, frameHeight: 128 });
     this.load.spritesheet('PsibotM', '/assets/images/sprites/avatars/4351.png', { frameWidth: 64, frameHeight: 64 });
     this.load.spritesheet('PsibotM_slash', '/assets/images/sprites/avatars/4351.png', { frameWidth: 192, frameHeight: 192 });
     this.load.spritesheet('otherPlayer', '/assets/images/sprites/avatars/4351.png', { frameWidth: 64, frameHeight: 64 });
-
     this.load.image('ship', '/assets/images/star_gold.png');
     this.load.image('book', '/assets/images/book.png');
     this.load.image('star', '/assets/images/star_gold.png');
@@ -28,7 +25,6 @@ export class BootScene extends Scene {
     this.load.image('portal00002', '/assets/images/enemyBlack5.png');
     this.load.image('portal00003', '/assets/images/enemyBlack5.png');
     this.load.image('reward', '/assets/images/various-32-greyout_69.png');
-
     this.load.image('nextPage', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/assets/images/arrow-down-left.png');
     this.load.addFile(new WebFont(this.load, ['Roboto', 'Neutron Demo']))
     this.load.image('cursor', '/assets/images/cursors/blank.cur');
