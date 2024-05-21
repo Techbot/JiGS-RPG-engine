1b8139db1e3397f78c413fd5bc66bfc24f510baf
techbot <djtechbot@gmail.com>
2024-05-21 15:17:19 +0200

    bugfix

--- server/colliders/portalCollider.ts
+++ server/colliders/portalCollider.ts
@@ -9,14 +9,14 @@ var playerModel = require('../models/player.ts');
 export class portalCollider {
 
   do(room, bodyA, bodyB) {
-    console.log('portal ');
-    if (!bodyA.done) {
+    if (bodyA.isPortal) {
+        console.log('portal ');
+      if (!bodyA.done) {
+        //    const missionAccepted = room.state.missionAccepted;
+        //    const player = bodyB.client_id;
+        //    const playerFlags = bodyB.flags;
 
-  //    const missionAccepted = room.state.missionAccepted;
-  //    const player = bodyB.client_id;
-  //    const playerFlags = bodyB.flags;
-
-  //    if (playerFlags.includes(missionAccepted)) {
+        //    if (playerFlags.includes(missionAccepted)) {
         const promise1 = Promise.resolve(playerModel.updateMap(bodyB.profileId, bodyA.destination));
         promise1
           .then(() => { bodyB.portal = bodyA.tiled; })// defining .portal triggers the jump for the client
@@ -29,7 +29,8 @@ export class portalCollider {
             playerModel.updatePlayer(bodyB.profileId, 'y', bodyA.destination_y, 1)
           });
         bodyA.done = true;
-   //   }
+        //   }
+      }
     }
   }
 }
